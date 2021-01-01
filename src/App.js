75fb032909aadc0419192c698db018dda5ae5a4b
gudtjr3437 <gudtjr3437@naver.com>
2021-01-01 12:19:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-
-
+import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component{
     state = {
       isLoading : true ,
@@ -15,17 +15,38 @@ class App extends React.Component{
             movies
           }
         }
-      } = await axios.get("https://yts.mx/api/v2/list_movies.json");
-      this.setState({movies:movies}) //state에 있는 movies : axios에서 가져온 movies
+      } = await axios.get("https://yts.mx/api/v2/list_movies.json?sortby=rating");
+      this.setState({movies , isLoading:false}); //state에 있는 movies : axios에서 가져온 movies
     }
     componentDidMount(){
      this.getMovies();
   }
     render(){
-     const{isLoading} =this.state;
-     return<div>{isLoading ? "Loading..." : "We are ready"}</div>
-    }
-      
+     const{isLoading, movies} =this.state;
+     return(
+        <section class = "container">
+          {isLoading 
+          ? (<div class ="loader">
+            <span class ="loader__text">Loading...</span>
+       </div>
+      ):( 
+        <div class = "movies">
+          {
+            movies.map(movie =>(
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}
+                />
+              ))}
+        </div>
+      )}
+      </section>
+    );
+    }   
   }
   
 
