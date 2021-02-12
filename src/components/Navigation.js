930e2c365461dbdd8a9b650310b2ae0beb3e038c
gudtjr3437 <gudtjr3437@naver.com>
2021-02-12 20:51:13 +0900

    Add Detail page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import "./Navigation.css"
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
-function Navigation(){
-    return(
+function Navigation() {
+  return (
     <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </div>
-    );
+  );
 }
 
 export default Navigation;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,32 @@
 import React from "react";
-
-function Detail(props){
-    console.log(props);
-    return<div>hello</div>;
+import "./Detail.css"
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <div className="detail__container">
+          <div className="container__data">
+      <div className="detail__title">{location.state.title}</div>
+      <div className="detail__year">{location.state.year}</div>
+      <img className="detail__poster" src={location.state.poster} alt={location.state.title}/>
+      <div className="detail__genres">{location.state.genres}</div>
+      </div>
+      <div className="detail__summary_container">
+        <div>{location.state.summary}</div>
+        </div>
+      </div>
+      );
+      
+    } else {
+      return null;
+    }
+  }
 }
-
 export default Detail;
