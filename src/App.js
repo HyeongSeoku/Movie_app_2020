d8f335b30a9c6a3a1c06a11c896c4dae6447b0a3
gudtjr3437 <gudtjr3437@naver.com>
2021-02-12 15:00:21 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import {HashRouter , Route} from "react-router-dom"
+import About from "./routes/About"
+import Home from "./routes/Home"
 
-
+//네비게이터가 두개의 컴포넌트를 렌더링하는 현상 (/일때) 을 막기위해 exact ={true} 설정 
 function App(){
-  return <span>something</span>
+  return (
+  <HashRouter>
+    <Route path="/" exact={true} component={Home}/>   
+    <Route path="/about" component={About}/>
+  </HashRouter>
+  );
 }
 
 export default App;
