159ff51003b859f01ecfcd7ebf2bba1d2aec5e97
gudtjr3437 <gudtjr3437@naver.com>
2021-02-12 15:48:48 +0900

    #6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import {HashRouter , Route} from "react-router-dom"
 import About from "./routes/About"
 import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
+import Detail from "./routes/Detail"
+import "./App.css"
 
 //네비게이터가 두개의 컴포넌트를 렌더링하는 현상 (/일때) 을 막기위해 exact ={true} 설정 
 function App(){
   return (
   <HashRouter>
+    <Navigation/>
     <Route path="/" exact={true} component={Home}/>   
     <Route path="/about" component={About}/>
+    <Route path="/movie-detail" component={Detail}/>
+
   </HashRouter>
   );
 }
